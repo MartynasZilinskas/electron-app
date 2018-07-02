a324fa844c7e4a7b07d7a67e3f5ac769bcc289b7
Martynas Žilinskas <martynas@quatrodev.com>
2018-07-02 17:43:35 +0300

    Releasing/Publishing updated package.json.

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow } from "electron";
+import { autoUpdater } from "electron-updater";
 import * as url from "url";
 import * as path from "path";
 
 const IS_SERVE: boolean = process.argv.indexOf("--serve") !== -1;
 const DEV_PORT: number = 4000;
 
-app.on("ready", () => {
+app.on("ready", async () => {
     const window = new BrowserWindow();
 
     if (IS_SERVE) {
@@ -23,4 +24,6 @@ app.on("ready", () => {
         );
         window.webContents.openDevTools();
     }
+    const result = await autoUpdater.checkForUpdates();
+    console.log(await result.downloadPromise!);
 });
