930feeda588f09e0a973ad08e95c16a7f98ce8a6
Martynas Žilinskas <martynas@quatrodev.com>
2018-07-02 15:20:32 +0300

    Updated packaging.

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from "electron";
+import * as url from "url";
+import * as path from "path";
 
 const IS_SERVE: boolean = process.argv.indexOf("--serve") !== -1;
 const DEV_PORT: number = 4000;
@@ -7,6 +9,18 @@ app.on("ready", () => {
     const window = new BrowserWindow();
 
     if (IS_SERVE) {
+        require("electron-reload")(__dirname, {
+            electron: require(`${__dirname}/node_modules/electron`)
+        });
         window.loadURL(`http://localhost:${DEV_PORT}`);
+    } else {
+        window.loadURL(
+            url.format({
+                pathname: path.join(__dirname, "dist/renderer/index.html"),
+                protocol: "file:",
+                slashes: true
+            })
+        );
+        window.webContents.openDevTools();
     }
 });
--- renderer/webpack.config.js
+++ renderer/webpack.config.js
@@ -1,8 +1,15 @@
 const { generateWebpackConfig } = require("@simplrjs/webpack");
 
-module.exports = generateWebpackConfig({
+const config = generateWebpackConfig({
     devServerPort: 4000,
     entryFile: "./src/app.ts",
     outputDirectory: "../dist/renderer",
     projectDirectory: __dirname
 });
+
+config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)\/(webpack-dev-server)/
+});
+
+module.exports = config;
